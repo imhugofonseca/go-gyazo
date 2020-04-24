9e8b73cc0aa853d60cbb9640480dd1ff82f92e52
Hugo <hugo.fonseca@luggit.app>
2020-04-25 00:55:24 +0100

    chore: code cleanup

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 
 	"github.com/minio/minio-go"
 	"github.com/segmentio/ksuid"
@@ -20,25 +19,20 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var buf bytes.Buffer
-	file, header, err := r.FormFile("imagedata")
+	file, _, err := r.FormFile("imagedata")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-
-	name := strings.Split(header.Filename, ".")
-	fmt.Printf("File name %s\n", name[0])
-
 	io.Copy(&buf, file)
-
 	hash := ksuid.New().String()
 
-	fmt.Println(hash)
-	accessKey := os.Getenv("DO_ACCESS_KEY")
-	secKey := os.Getenv("DO_SECRET_ACCESS_KEY")
-	ssl := true
+	host := os.Getenv("BKT_HOST")
+	accessKey := os.Getenv("BKT_ACCESS_KEY")
+	secKey := os.Getenv("BKT_SECRET_ACCESS_KEY")
+	spaceDomain := os.Getenv("BKT_SPACE_DOMAIN")
 
-	client, err := minio.New("fra1.digitaloceanspaces.com", accessKey, secKey, ssl)
+	client, err := minio.New(host, accessKey, secKey, true)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,21 +41,15 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	fileName := fmt.Sprintf("%s.png", hash)
 	contentType := "image/png"
 	userMetaData := map[string]string{"x-amz-acl": "public-read"}
-
-	fmt.Println(fileName)
-
-	n, err := client.PutObject("hugoimg", fileName, reader, reader.Size(), minio.PutObjectOptions{
+	_, err = client.PutObject("hugoimg", fileName, reader, reader.Size(), minio.PutObjectOptions{
 		ContentType:  contentType,
 		UserMetadata: userMetaData,
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(n)
 	buf.Reset()
-
-	fmt.Fprintf(w, "%s/%s", "https://img.hugo.gg", fileName)
+	fmt.Fprintf(w, "https://%s/%s", spaceDomain, fileName)
 }
 
 func main() {
